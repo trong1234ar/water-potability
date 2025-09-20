ebd37fd34b13d946789598f231b4fefea0b1524d
trong1234ar <trong224466@gmail.com>
2025-09-20 11:20:08 +0700

    fix username

--- src/model/model_reg.py
+++ src/model/model_reg.py
@@ -21,7 +21,7 @@ dagshub_token = os.getenv("DAGSHUB_TOKEN")
 if not dagshub_token:
     raise ValueError("DAGSHUB_TOKEN environment variable is not set")
 # Set environment variables for MLflow authentication
-os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = "trong1234ar"
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 # Configure DagsHub MLflow tracking
 dagshub_url = "https://dagshub.com"
